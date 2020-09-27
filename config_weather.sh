f159e4d20e4066e1ce6befc74186e93d90e2d9d6
Seamus Kane <seamuskane@aim.com>
2020-09-27 15:44:24 +0100

    Create config_weather.sh

---
+++ config_weather.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+export ow_api="your_open_weather_api"
+#Portaferry
+#export lat=54.38
+#export lon=-5.54
+
+#moscow
+#export lat=55.7558
+#export lon=37.6173
+
+#Bangkok
+#export lat=13.7563
+#export lon=100.5231
+
+
+#Durban/Coordinates
+#export lat=-29.8587
+#export lon=31.0218
+
+#Uttar Pradesh
+#export lat=28.207609
+#export lon=79.826660
+
+#Karachi
+#export lat=24.860966
+#export lon=66.990501
+
+#Jamnagar/Coordinates
+export lat=22.4707
+export lon=70.0577
